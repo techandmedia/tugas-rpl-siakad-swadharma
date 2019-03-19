23938dd0f199ebc654246adb03695c791285c0fa
Eko Andri Subarnanto <eko.andri@icloud.com>
2019-03-19 19:21:23 +0700

    rearranging server folder, adding nodemon json

--- server/config/query-sample.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `t1` (`id`, `ts`, `dt`) VALUES (NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
--- update tabel t1 untuk field name
--- semua kolom dengan parameter on update akan terupdate
--- contoh kolom modified date akan terupdate secara otomatis
--- walaupun kolom modified date tidak termasuk di dalam query update
--- contoh berikut hanya melakukan update untuk kolom nama
-UPDATE `t1` SET name="Eko Andri S" WHERE id=0
